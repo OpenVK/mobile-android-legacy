e1cff751256d34c2c411adc0d4ab2ca2a71d174a
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2023-07-07 22:10:07 +0700

    Update AppActivity.java

--- app/src/main/java/uk/openvk/android/legacy/ui/core/activities/AppActivity.java
+++ app/src/main/java/uk/openvk/android/legacy/ui/core/activities/AppActivity.java
@@ -27,8 +27,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
