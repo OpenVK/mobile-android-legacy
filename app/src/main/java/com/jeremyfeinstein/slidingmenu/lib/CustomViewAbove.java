e00c13cef955908def1eaa6e02883cef54a66714
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2023-07-09 17:29:51 +0700

    Fix funny crash

--- app/src/main/java/com/jeremyfeinstein/slidingmenu/lib/CustomViewAbove.java
+++ app/src/main/java/com/jeremyfeinstein/slidingmenu/lib/CustomViewAbove.java
@@ -828,11 +828,15 @@ public class CustomViewAbove extends ViewGroup {
 
 	@Override
 	protected void dispatchDraw(Canvas canvas) {
-		super.dispatchDraw(canvas);
-		// Draw the margin drawable if needed.
-		mViewBehind.drawShadow(mContent, canvas);
-		mViewBehind.drawFade(mContent, canvas, getPercentOpen());
-		mViewBehind.drawSelector(mContent, canvas, getPercentOpen());
+		try {
+			super.dispatchDraw(canvas);
+			// Draw the margin drawable if needed.
+			mViewBehind.drawShadow(mContent, canvas);
+			mViewBehind.drawFade(mContent, canvas, getPercentOpen());
+			mViewBehind.drawSelector(mContent, canvas, getPercentOpen());
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 
 	// variables for drawing
