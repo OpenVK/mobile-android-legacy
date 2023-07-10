1b8420866df997c1ffa88b9bb9d1999f4db219c7
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2023-07-10 20:42:52 +0700

    Update MainSettingsFragment.java

--- app/src/main/java/uk/openvk/android/legacy/ui/core/fragments/app/MainSettingsFragment.java
+++ app/src/main/java/uk/openvk/android/legacy/ui/core/fragments/app/MainSettingsFragment.java
@@ -397,7 +397,7 @@ public class MainSettingsFragment extends PreferenceFragmentCompatDividers {
         dialog.show();
     }
 
-    private void openLogoutConfirmationDialog() {
+    public void openLogoutConfirmationDialog() {
         OvkAlertDialog logout_dlg;
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         builder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
