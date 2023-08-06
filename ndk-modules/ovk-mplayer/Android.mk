193e4a756287c570d402478fea757696edfa7c20
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2023-08-06 12:43:59 +0700

    Fix

--- ndk-modules/ovk-mplayer/Android.mk
+++ ndk-modules/ovk-mplayer/Android.mk
@@ -5,17 +5,26 @@ LOCAL_PATH := $(call my-dir)
 
 #declare the prebuilt library
 
+
+ifeq($(ANDROID_ARCH_ABI),armeabi)
+	ANDROID_ARCH = armv6
+ifeq($(ANDROID_ARCH_ABI),armeabi-v7a)
+	ANDROID_ARCH = armv7
+else
+	ANDROID_ARCH = armv8
+endif
+
 include $(CLEAR_VARS)
 
 FFMPEG_VERSION := 4.0.4
 
 LOCAL_MODULE := ffmpeg-prebuilt
 
-LOCAL_SRC_FILES := ../ffmpeg-android-builder/ffmpeg/android/${ANDROID_ABI}/libffmpeg-v${FFMPEG_VERSION}.so
+LOCAL_SRC_FILES := ../ffmpeg-android-builder/ffmpeg/android/$(ANDROID_ARCH)/libffmpeg-v${FFMPEG_VERSION}.so
 
-LOCAL_EXPORT_C_INCLUDES := ../ffmpeg-android-builder/ffmpeg/android/${ANDROID_ABI}/include
+LOCAL_EXPORT_C_INCLUDES := ../ffmpeg-android-builder/ffmpeg/android/$(ANDROID_ARCH)/include
 
-LOCAL_EXPORT_LDLIBS := ../ffmpeg-android-builder/ffmpeg/android/${ANDROID_ABI}/libffmpeg-v${FFMPEG_VERSION}.so
+LOCAL_EXPORT_LDLIBS := ../ffmpeg-android-builder/ffmpeg/android/$(ANDROID_ARCH)/libffmpeg-v${FFMPEG_VERSION}.so
 
 LOCAL_PRELINK_MODULE := true
 
@@ -25,9 +34,9 @@ include $(CLEAR_VARS)
 LOCAL_ALLOW_UNDEFINED_SYMBOLS=false
 LOCAL_MODULE := ovkmplayer
 LOCAL_SRC_FILES := ovkmplayer
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../ffmpeg-android-builder/ffmpeg/android/${ANDROID_ABI}/include
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../ffmpeg-android-builder/ffmpeg/android/$(ANDROID_ARCH)/include
 LOCAL_SHARED_LIBRARY := ffmpeg-prebuilt
-LOCAL_LDLIBS    := -llog -ljnigraphics -lz -lm $(LOCAL_PATH)/../ffmpeg-android-builder/ffmpeg/android/${ANDROID_ABI}/libffmpeg-v${FFMPEG_VERSION}.so
+LOCAL_LDLIBS    := -llog -ljnigraphics -lz -lm $(LOCAL_PATH)/../ffmpeg-android-builder/ffmpeg/android/$(ANDROID_ARCH)/libffmpeg-v${FFMPEG_VERSION}.so
 
 include $(BUILD_SHARED_LIBRARY)
 
