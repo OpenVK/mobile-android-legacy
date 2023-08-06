291dd5d00b35ab8240a8b87dd5420b636d73b085
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2023-08-06 12:29:21 +0700

    combining lib

--- ndk-modules/ovk-mplayer/Application.mk
+++ ndk-modules/ovk-mplayer/Application.mk
@@ -14,33 +14,6 @@
 #
 #  Source code: https://github.com/tinelix/ffmpeg-android-builder
 
-LOCAL_PATH := $(call my-dir)
-
-#declare the prebuilt library
-
-include $(CLEAR_VARS)
-
-FFMPEG_VERSION := 4.0.4
-
-LOCAL_MODULE := ffmpeg-prebuilt
-
-LOCAL_SRC_FILES := ../ffmpeg/android/armv7/libffmpeg-v${FFMPEG_VERSION}.so
-
-LOCAL_EXPORT_C_INCLUDES := ../ffmpeg/android/armv6/include
-
-LOCAL_EXPORT_LDLIBS := ../ffmpeg/android/armv7/libffmpeg-v${FFMPEG_VERSION}.so
-
-LOCAL_PRELINK_MODULE := true
-
-include $(PREBUILT_SHARED_LIBRARY)
-include $(CLEAR_VARS)
-
-LOCAL_ALLOW_UNDEFINED_SYMBOLS=false
-LOCAL_MODULE := ffmpeg-player
-LOCAL_SRC_FILES := ffmpeg-player.c
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../ffmpeg/android/armv7-a/include
-LOCAL_SHARED_LIBRARY := ffmpeg-prebuilt
-LOCAL_LDLIBS    := -llog -ljnigraphics -lz -lm $(LOCAL_PATH)/../ffmpeg/android/armv7/libffmpeg-v${FFMPEG_VERSION}.so
-
-include $(BUILD_SHARED_LIBRARY)
+APP_ABI := armeabi, armeabi-v7a, arm64-v8a
+APP_PLATFORM := android-5
 
