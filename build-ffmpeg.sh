532a9cff2296e4124bb827f16a18ccc15db3e5e5
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2023-08-07 14:18:12 +0700

    Update build-ffmpeg.sh

--- build-ffmpeg.sh
+++ build-ffmpeg.sh
@@ -9,13 +9,24 @@ git submodule update
 cd ndk-modules/ffmpeg-builder-0.8.5
 git checkout -b ffmpeg-0.8.5
 chmod -R 0777 . && chmod +x ./build-android.sh
+
+# Build FFmpeg 0.8.5 & 4.0.4
 ./build-android.sh armv6
 ./build-android.sh armv7
 # FFmpeg 0.8.5 not buildable under ARMv8a 64-bit
 cd ../ffmpeg-builder-4.0.4
 chmod -R 0777 . && chmod +x ./build-android.sh
 ./build-android.sh armv8a
-cp -R *.so ./ffmpeg/android ../../app/src/main/jniLibs
+
+# Create directories in jniLibs
+mkdir ../../app/src/main/jniLibs/armeabi
+mkdir ../../app/src/main/jniLibs/armeabi-v7a
+mkdir ../../app/src/main/jniLibs/arm64-v8a
+
+echo "OpenVK Legacy | Copying libraries to project..."
+echo;
+cp ./ffmpeg/android/armeabi/libffmpeg-0.8.5.so ../../app/src/main/jniLibs/armeabi
+cp ./ffmpeg/android/armeabi-v7a/libffmpeg-0.8.5.so ../../app/src/main/jniLibs/armeabi-v7a
 cd ../ffmpeg-builder-0.8.5
-cp -R *.so ./ffmpeg/android ../../app/src/main/jniLibs
+cp ./ffmpeg/android/arm64-v8a/libffmpeg-4.0.4.so ../app/src/main/jniLibs/arm64-v8a
 echo "OpenVK Legacy | Done!"
