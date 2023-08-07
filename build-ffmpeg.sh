1b12d5a78d96483638879df764080ce582d8bb0f
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2023-08-07 13:44:55 +0700

    Update build-ffmpeg.sh

--- build-ffmpeg.sh
+++ build-ffmpeg.sh
@@ -11,6 +11,8 @@ git checkout -b ffmpeg-0.8.5
 chmod -R 0777 . && chmod +x ./build-android.sh
 ./build-android.sh armv6
 ./build-android.sh armv7
+# FFmpeg 0.8.5 not buildable under ARMv8a 64-bit
+git checkout -b ffmpeg-4.0.4
 ./build-android.sh armv8a
 cp -R *.so ./ffmpeg/android ../../app/src/main/jniLibs
 echo "OpenVK Legacy | Done!"
