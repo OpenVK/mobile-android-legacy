727dc317ddc71d94f3cf60a8e7563b1f9d88c3e4
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2023-08-07 12:14:21 +0700

    Update build-ffmpeg.sh

--- build-ffmpeg.sh
+++ build-ffmpeg.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+#  Automatic FFmpeg 0.8.5 for Android build script using builder by Tinelix
+#  FFmpeg and FFmpeg for Android builder by Tinelix licensed under LGPLv3 or later version.
+
+echo "OpenVK Legacy | FFmpeg for Android building..."
 git submodule init
 git submodule update
 cd ndk-modules/ffmpeg-android-builder
-chmod +x ./build-android.sh
-./build-android.sh
+chmod -R 0777 . && chmod +x ./build-android.sh
+./build-android.sh armv6
+./build-android.sh armv7
+./build-android.sh armv8a
+cp -R ./ffmpeg/android ../../app/src/main/jniLibs
