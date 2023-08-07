79b831c2078c2bc13d6da1c3c30eba173915ca17
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2023-08-07 12:27:42 +0700

    Update build-ffmpeg.sh

--- build-ffmpeg.sh
+++ build-ffmpeg.sh
@@ -6,6 +6,7 @@ echo "OpenVK Legacy | FFmpeg for Android building..."
 echo;
 git submodule init
 git submodule update
+git checkout -b ffmpeg-0.8.5
 cd ndk-modules/ffmpeg-android-builder
 chmod -R 0777 . && chmod +x ./build-android.sh
 ./build-android.sh armv6
