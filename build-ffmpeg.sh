74a6b52b4979423312169823edc5fc4295c92429
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2023-08-08 18:27:54 +0700

    Update build-ffmpeg.sh

--- build-ffmpeg.sh
+++ build-ffmpeg.sh
@@ -6,7 +6,7 @@ echo "OpenVK Legacy | Downloading from https://github.com/tinelix/ffmpeg-android
 cd ndk-modules/ovkmplayer
 git clone https://github.com/tinelix/ffmpeg-android-builder builder
 cd builder
-chmod -R 0777 . && chmod +x ./build-android-2.3.sh && chmod +x ./build-android-4.0.4.sh
+chmod -R 0777 . && chmod +x ./build-android-2.6.sh && chmod +x ./build-android-4.0.4.sh
 
 echo "OpenVK Legacy | FFmpeg for Android building..."
 # argument == ffmpeg version, if not specified compiling all versions
@@ -19,11 +19,11 @@ if [[ -z $1 ]]; then
 elif [ $1 == "0.8.5" ]; then
     ./build-android-0.8.5.sh armv6 r8e
     ./build-android-0.8.5.sh armv7 r8e
-elif [ $1 == "2.3" ]; then
-    ./build-android-2.3.sh armv8a
+elif [ $1 == "2.6" ]; then
+    ./build-android-2.6.sh armv8a
 else
     echo;
-    echo "[ERROR] Wrong argument: ./build-ffmpeg.sh [version == '2.3' or '4.0.4']"
+    echo "[ERROR] Wrong argument: ./build-ffmpeg.sh [version == '2.6' or '4.0.4']"
     exit 1;
 fi;
 
@@ -36,14 +36,14 @@ echo "OpenVK Legacy | Copying libraries to project..."
 echo;
 
 if [[ -z $1 ]]; then
-    cp ./ffmpeg-2.3/android/arm64-v8a/libffmpeg.so ../../app/src/main/jniLibs/arm64-v8a
+    cp ./ffmpeg-2.6/android/arm64-v8a/libffmpeg.so ../../app/src/main/jniLibs/arm64-v8a
     cp ./ffmpeg-0.8.5/android/armeabi/libffmpeg.so ../../app/src/main/jniLibs/armeabi
     cp ./ffmpeg-0.8.5/android/armeabi-v7a/libffmpeg.so ../../app/src/main/jniLibs/armeabi-v7a
 elif [ $1 == "0.8.5" ]; then
     cp ./ffmpeg-0.8.5/android/armeabi/libffmpeg.so ../../app/src/main/jniLibs/armeabi
     cp ./ffmpeg-0.8.5/android/armeabi-v7a/libffmpeg.so ../../app/src/main/jniLibs/armeabi-v7a
 else
-    cp ./ffmpeg-2.3/android/arm64-v8a/libffmpeg.so ../../app/src/main/jniLibs/arm64-v8a
+    cp ./ffmpeg-2.6/android/arm64-v8a/libffmpeg.so ../../app/src/main/jniLibs/arm64-v8a
 fi;
 
 echo "OpenVK Legacy | Done!"
