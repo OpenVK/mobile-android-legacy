293b3385c538d36a5b395bfdb2d451f1cba21a37
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2023-08-08 14:16:10 +0700

    Update build-ffmpeg.sh

--- build-ffmpeg.sh
+++ build-ffmpeg.sh
@@ -4,28 +4,21 @@
 
 echo "OpenVK Legacy | FFmpeg for Android building..."
 echo;
-git submodule init
-git submodule update
-cd ndk-modules/ffmpeg-builder-0.8.5
-git checkout -b ffmpeg-0.8.5
-chmod -R 0777 . && chmod +x ./build-android.sh
+cd ndk-modules/ovkmplayer
+chmod -R 0777 . && chmod +x ./build-android-0.8.5.sh && chmod +x ./build-android-4.0.4.sh
 
 # argument == ffmpeg version, if not specified compiling all versions
 if [[ -z $1 ]]; then
     # Build FFmpeg 0.8.5 & 4.0.4
-    ./build-android.sh armv6
-    ./build-android.sh armv7
+    ./build-android-0.8.5.sh armv6
+    ./build-android-0.8.5.sh armv7
     # FFmpeg 0.8.5 not buildable under ARMv8a 64-bit
-    cd ../ffmpeg-builder-4.0.4
-    chmod -R 0777 . && chmod +x ./build-android.sh
-    ./build-android.sh armv8a
+    ./build-android-4.0.4.sh armv8a
 elif [ $1 == "0.8.5" ]; then
-    ./build-android.sh armv6 r8e
-    ./build-android.sh armv7 r8e
+    ./build-android-0.8.5.sh armv6 r8e
+    ./build-android-0.8.5.sh armv7 r8e
 elif [ $1 == "4.0.4" ]; then
-    cd ../ffmpeg-builder-4.0.4
-    chmod -R 0777 . && chmod +x ./build-android.sh
-    ./build-android.sh armv8a
+    ./build-android-4.0.4.sh armv8a
 else
     echo;
     echo "[ERROR] Wrong argument: ./build-ffmpeg.sh [version == '0.8.5' or '4.0.4']"
@@ -41,17 +34,14 @@ echo "OpenVK Legacy | Copying libraries to project..."
 echo;
 
 if [[ -z $1 ]]; then
-    cp ./ffmpeg/android/arm64-v8a/libffmpeg.so ../../app/src/main/jniLibs/arm64-v8a
-    cp ./ffmpeg/android/arm64-v8a/include ../../app/src/main/jniLibs/arm64-v8a
-    cd ../ffmpeg-builder-0.8.5
-    cp ./ffmpeg/android/armeabi/libffmpeg.so ../../app/src/main/jniLibs/armeabi
-    cp ./ffmpeg/android/armeabi-v7a/libffmpeg.so ../../app/src/main/jniLibs/armeabi-v7a
+    cp ./ffmpeg-4.0.4/android/arm64-v8a/libffmpeg.so ../../app/src/main/jniLibs/arm64-v8a
+    cp ./ffmpeg-0.8.5/android/armeabi/libffmpeg.so ../../app/src/main/jniLibs/armeabi
+    cp ./ffmpeg-0.8.5/android/armeabi-v7a/libffmpeg.so ../../app/src/main/jniLibs/armeabi-v7a
 elif [ $1 == "0.8.5" ]; then
-    cd ../ffmpeg-builder-0.8.5
-    cp ./ffmpeg/android/armeabi/libffmpeg.so ../../app/src/main/jniLibs/armeabi
-    cp ./ffmpeg/android/armeabi-v7a/libffmpeg.so ../../app/src/main/jniLibs/armeabi-v7a
+    cp ./ffmpeg-0.8.5/android/armeabi/libffmpeg.so ../../app/src/main/jniLibs/armeabi
+    cp ./ffmpeg-0.8.5/android/armeabi-v7a/libffmpeg.so ../../app/src/main/jniLibs/armeabi-v7a
 else
-    cp ./ffmpeg/android/arm64-v8a/libffmpeg.so ../../app/src/main/jniLibs/arm64-v8a
+    cp ./ffmpeg-4.0.4/android/arm64-v8a/libffmpeg.so ../../app/src/main/jniLibs/arm64-v8a
 fi;
 
 echo "OpenVK Legacy | Done!"
