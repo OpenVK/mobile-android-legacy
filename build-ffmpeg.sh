0d7efb6e26dbcd3cb5d84fc2e98adc6b5b98f7a7
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2023-08-11 09:53:20 +0700

    Update build-ffmpeg.sh

--- build-ffmpeg.sh
+++ build-ffmpeg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#  FFmpeg 0.8.5 & 3.1.4 for Android automatic build script using builder by Tinelix
+#  FFmpeg 0.8.12 & 3.1.4 for Android automatic build script using builder by Tinelix
 #  NOTICE: FFmpeg and FFmpeg for Android builder by Tinelix licensed under LGPLv3 or later version.
 
 echo "OpenVK Legacy | Downloading from https://github.com/tinelix/ffmpeg-android-builder..."
@@ -11,21 +11,21 @@ chmod -R 0777 . && chmod +x ./build-android-3.1.4.sh && chmod +x ./build-android
 echo "OpenVK Legacy | FFmpeg for Android building..."
 # argument == ffmpeg version, if not specified compiling all versions
 if [[ -z $1 ]]; then
-    # Build FFmpeg 0.8.5 & 3.1.4 for 32-bit architecture
-    ./build-android-0.8.5.sh armv6 r8e
-    ./build-android-0.8.5.sh armv7 r8e
-    ./build-android-0.8.5.sh x86 r8e
-    # FFmpeg 0.8.5 not buildable under ARMv8a 64-bit and x64
+    # Build FFmpeg 0.8.12 & 3.1.4 for 32-bit architecture
+    ./build-android-0.8.12.sh armv6 r8e
+    ./build-android-0.8.12.sh armv7 r8e
+    ./build-android-0.8.12.sh x86 r8e
+    # FFmpeg 0.8.12 not buildable under ARMv8a 64-bit and x64
     ./build-android-3.1.4.sh armv8a
-elif [ $1 == "0.8.5" ]; then
-    ./build-android-0.8.5.sh armv6 r8e
-    ./build-android-0.8.5.sh armv7 r8e
-    ./build-android-0.8.5.sh x86 r8e
+elif [ $1 == "0.8.12" ]; then
+    ./build-android-0.8.12.sh armv6 r8e
+    ./build-android-0.8.12.sh armv7 r8e
+    ./build-android-0.8.12.sh x86 r8e
 elif [ $1 == "3.1.4" ]; then
     ./build-android-3.1.4.sh armv8a
 else
     echo;
-    echo "[ERROR] Wrong argument: ./build-ffmpeg.sh [version == '3.1.4' or '0.8.5']"
+    echo "[ERROR] Wrong argument: ./build-ffmpeg.sh [version == '3.1.4' or '0.8.12']"
     exit 1;
 fi;
 
@@ -40,13 +40,13 @@ echo;
 
 if [[ -z $1 ]]; then
     cp ./ffmpeg-3.1.4/android/arm64-v8a/libffmpeg.so ../../../app/src/main/jniLibs/arm64-v8a
-    cp ./ffmpeg-0.8.5/android/armeabi/libffmpeg.so ../../../app/src/main/jniLibs/armeabi
-    cp ./ffmpeg-0.8.5/android/armeabi-v7a/libffmpeg.so ../../../app/src/main/jniLibs/armeabi-v7a
-    cp ./ffmpeg-0.8.5/android/x86/libffmpeg.so ../../../app/src/main/jniLibs/x86
-elif [ $1 == "0.8.5" ]; then
-    cp ./ffmpeg-0.8.5/android/armeabi/libffmpeg.so ../../../app/src/main/jniLibs/armeabi
-    cp ./ffmpeg-0.8.5/android/armeabi-v7a/libffmpeg.so ../../../app/src/main/jniLibs/armeabi-v7a
-    cp ./ffmpeg-0.8.5/android/x86/libffmpeg.so ../../../app/src/main/jniLibs/x86
+    cp ./ffmpeg-0.8.12/android/armeabi/libffmpeg.so ../../../app/src/main/jniLibs/armeabi
+    cp ./ffmpeg-0.8.12/android/armeabi-v7a/libffmpeg.so ../../../app/src/main/jniLibs/armeabi-v7a
+    cp ./ffmpeg-0.8.12/android/x86/libffmpeg.so ../../../app/src/main/jniLibs/x86
+elif [ $1 == "0.8.12" ]; then
+    cp ./ffmpeg-0.8.12/android/armeabi/libffmpeg.so ../../../app/src/main/jniLibs/armeabi
+    cp ./ffmpeg-0.8.12/android/armeabi-v7a/libffmpeg.so ../../../app/src/main/jniLibs/armeabi-v7a
+    cp ./ffmpeg-0.8.12/android/x86/libffmpeg.so ../../../app/src/main/jniLibs/x86
 else
     cp ./ffmpeg-3.1.4/android/arm64-v8a/libffmpeg.so ../../../app/src/main/jniLibs/arm64-v8a
 fi;
