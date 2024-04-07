62d9cf2e9b7d4709e5cee40414d9bad44f75d28b
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2024-04-07 11:50:47 +0700

    feat(photo): Set translucent status bar on devices with screen cutouts

--- app/src/main/java/uk/openvk/android/legacy/core/activities/PhotoViewerActivity.java
+++ app/src/main/java/uk/openvk/android/legacy/core/activities/PhotoViewerActivity.java
@@ -212,7 +212,13 @@ public class PhotoViewerActivity extends NetworkActivity {
                                 getActionBar().hide();
                             } else {
                                 enableFullScreenMode();
-                                getActionBar().show();
+                                handler.postDelayed(new Runnable() {
+                                    @SuppressLint("NewApi")
+                                    @Override
+                                    public void run() {
+                                        getActionBar().show();
+                                    }
+                                }, 200);
                             }
                         } else {
                             if(actionBar.getVisibility() == View.VISIBLE) {
@@ -355,17 +361,26 @@ public class PhotoViewerActivity extends NetworkActivity {
         return true;
     }
 
+    @SuppressLint("NewApi")
     @Override
     protected void setTranslucentStatusBar() {
         resetTranslucentStatusBar();
-        super.setLegacyTranslucentStatusBar(1, Color.parseColor("#D8000000"));
+        if(Build.VERSION.SDK_INT == Build.VERSION_CODES.KITKAT)
+            super.setLegacyTranslucentStatusBar(1, Color.parseColor("#D8000000"));
+        else if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            getWindow().setStatusBarColor(Color.parseColor("#D8000000"));
+            getWindow().setStatusBarColor(Color.parseColor("#D8000000"));
+        } else
+            super.setLegacyTranslucentStatusBar(1, Color.parseColor("#A5000000"));
     }
 
     protected void enableFullScreenMode() {
-        if(!isFullScreenMode) {
+        if (!isFullScreenMode) {
             resetTranslucentStatusBar();
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
                 getWindow().getDecorView().setSystemUiVisibility(
                         View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -377,7 +392,7 @@ public class PhotoViewerActivity extends NetworkActivity {
         } else {
             getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
             setTranslucentStatusBar();
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
                 getWindow().getDecorView().setSystemUiVisibility(
                         View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
