4d398faf5b79a73936daeedff257cd2c64e7baba
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2024-04-14 10:06:50 +0700

    Add build parameters for Android NDK r8e and r10e

--- build-ffmpeg.sh
+++ build-ffmpeg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#  FFmpeg 1.1.12 for Android automatic build script using builder by Tinelix
+#  FFmpeg 2.2.4 & 1.1.12 for Android automatic build script using builder by Tinelix
 #
 #  NOTICE: FFmpeg and FFmpeg for Android builder by Tinelix licensed under LGPLv3 or later version.
 
@@ -16,10 +16,10 @@ chmod -R 0777 . && chmod +x ./build-android-1.1.12.sh
 
 echo "OpenVK Legacy | Starting FFmpeg builder..."
 
-./build-android-2.2.4.sh armv8a
-./build-android-1.1.12.sh armv7
-./build-android-1.1.12.sh armv6
-./build-android-1.1.12.sh x86
+ANDROID_NDK_HOME=${ANDROID_NDK_R10E} ./build-android-2.2.4.sh armv8a
+ANDROID_NDK_HOME=${ANDROID_NDK_R8E} ./build-android-1.1.12.sh armv7 r8e
+ANDROID_NDK_HOME=${ANDROID_NDK_R8E} ./build-android-1.1.12.sh armv6 r8e
+ANDROID_NDK_HOME=${ANDROID_NDK_R8E} ./build-android-1.1.12.sh x86 r8e
 
 # Create directories in jniLibs and ovkmplayer
 mkdir -p ../../../app/src/main/jniLibs/armeabi
@@ -30,7 +30,7 @@ mkdir -p ../../../app/src/main/jniLibs/x86
 echo "OpenVK Legacy | Copying libraries to project..."
 echo;
 
-cp ./ffmpeg-2.2.4/android/arm64-v8a/libffmpeg.so ../../../app/src/main/jniLibs/arm64-v8a
+cp ./ffmpeg-1.1.12/android/arm64-v8a/libffmpeg.so ../../../app/src/main/jniLibs/arm64-v8a
 cp ./ffmpeg-2.2.4/android/armeabi/libffmpeg.so ../../../app/src/main/jniLibs/armeabi
 cp ./ffmpeg-2.2.4/android/armeabi-v7a/libffmpeg.so ../../../app/src/main/jniLibs/armeabi-v7a
 cp ./ffmpeg-2.2.4/android/x86/libffmpeg.so ../../../app/src/main/jniLibs/x86
