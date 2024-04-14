057412ef78a69fc2e7216796f3b2809d9e329dd4
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2024-04-14 07:16:55 +0700

    Add Ffmpeg 2.5.11

--- build-ffmpeg.sh
+++ build-ffmpeg.sh
@@ -12,14 +12,14 @@ git submodule update
 cd ndk-modules/ovkmplayer
 cd builder
 git checkout origin/main
-chmod -R 0777 . && chmod +x ./build-android-3.1.4.sh
+chmod -R 0777 . && chmod +x ./build-android-2.5.11.sh
 
 echo "OpenVK Legacy | Starting FFmpeg builder..."
 
-./build-android-3.1.4.sh armv8a
-./build-android-3.1.4.sh armv7
-./build-android-3.1.4.sh armv6
-./build-android-3.1.4.sh x86
+./build-android-2.5.11.sh armv8a
+./build-android-2.5.11.sh armv7
+./build-android-2.5.11.sh armv6
+./build-android-2.5.11.sh x86
 
 # Create directories in jniLibs and ovkmplayer
 mkdir -p ../../../app/src/main/jniLibs/armeabi
@@ -30,9 +30,9 @@ mkdir -p ../../../app/src/main/jniLibs/x86
 echo "OpenVK Legacy | Copying libraries to project..."
 echo;
 
-cp ./ffmpeg-3.1.4/android/arm64-v8a/libffmpeg.so ../../../app/src/main/jniLibs/arm64-v8a
-cp ./ffmpeg-3.1.4/android/armeabi/libffmpeg.so ../../../app/src/main/jniLibs/armeabi
-cp ./ffmpeg-3.1.4/android/armeabi-v7a/libffmpeg.so ../../../app/src/main/jniLibs/armeabi-v7a
-cp ./ffmpeg-3.1.4/android/x86/libffmpeg.so ../../../app/src/main/jniLibs/x86
+cp ./ffmpeg-2.5.11/android/arm64-v8a/libffmpeg.so ../../../app/src/main/jniLibs/arm64-v8a
+cp ./ffmpeg-2.5.11/android/armeabi/libffmpeg.so ../../../app/src/main/jniLibs/armeabi
+cp ./ffmpeg-2.5.11/android/armeabi-v7a/libffmpeg.so ../../../app/src/main/jniLibs/armeabi-v7a
+cp ./ffmpeg-2.5.11/android/x86/libffmpeg.so ../../../app/src/main/jniLibs/x86
 
 echo "OpenVK Legacy | Done!"
