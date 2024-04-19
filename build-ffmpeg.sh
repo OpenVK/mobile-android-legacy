c5af82d6a04ff08bf47581ef775631e30944c018
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2024-04-19 07:19:46 +0700

    Revert to FFmpeg 2.8.11

--- build-ffmpeg.sh
+++ build-ffmpeg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#  FFmpeg 3.1.4 for Android automatic build script using builder by Tinelix
+#  FFmpeg 2.8.11 for Android automatic build script using builder by Tinelix
 #
 #  NOTICE: FFmpeg and FFmpeg for Android builder by Tinelix licensed under LGPLv3 or later version.
 
@@ -12,14 +12,14 @@ git submodule update
 cd ndk-modules/ovkmplayer
 cd builder
 git checkout origin/main
-chmod -R 0777 . && chmod +x ./build-android-3.1.4.sh
+chmod -R 0777 . && chmod +x ./build-android-2.8.11.sh
 
 echo "OpenVK Legacy | Starting FFmpeg builder..."
 
-ANDROID_NDK_HOME=${ANDROID_NDK_R10E} ./build-android-3.1.4.sh armv8a r10e
-ANDROID_NDK_HOME=${ANDROID_NDK_R8E} ./build-android-3.1.4.sh armv7 r8e
-ANDROID_NDK_HOME=${ANDROID_NDK_R8E} ./build-android-3.1.4.sh armv6 r8e
-ANDROID_NDK_HOME=${ANDROID_NDK_R8E} ./build-android-3.1.4.sh x86 r8e
+ANDROID_NDK_HOME=${ANDROID_NDK_R10E} ./build-android-2.8.11.sh armv8a r10e
+ANDROID_NDK_HOME=${ANDROID_NDK_R8E} ./build-android-2.8.11.sh armv7 r8e
+ANDROID_NDK_HOME=${ANDROID_NDK_R8E} ./build-android-2.8.11.sh armv6 r8e
+ANDROID_NDK_HOME=${ANDROID_NDK_R8E} ./build-android-2.8.11.sh x86 r8e
 
 # Create directories in jniLibs and ovkmplayer
 mkdir -p ../../../app/src/main/jniLibs/armeabi
@@ -30,9 +30,9 @@ mkdir -p ../../../app/src/main/jniLibs/x86
 echo "OpenVK Legacy | Copying libraries to project..."
 echo;
 
-cp ./ffmpeg-3.1.4/android/arm64-v8a/libffmpeg.so ../../../app/src/main/jniLibs/arm64-v8a
-cp ./ffmpeg-3.1.4/android/armeabi/libffmpeg.so ../../../app/src/main/jniLibs/armeabi
-cp ./ffmpeg-3.1.4/android/armeabi-v7a/libffmpeg.so ../../../app/src/main/jniLibs/armeabi-v7a
-cp ./ffmpeg-3.1.4/android/x86/libffmpeg.so ../../../app/src/main/jniLibs/x86
+cp ./ffmpeg-2.8.11/android/arm64-v8a/libffmpeg.so ../../../app/src/main/jniLibs/arm64-v8a
+cp ./ffmpeg-2.8.11/android/armeabi/libffmpeg.so ../../../app/src/main/jniLibs/armeabi
+cp ./ffmpeg-2.8.11/android/armeabi-v7a/libffmpeg.so ../../../app/src/main/jniLibs/armeabi-v7a
+cp ./ffmpeg-2.8.11/android/x86/libffmpeg.so ../../../app/src/main/jniLibs/x86
 
 echo "OpenVK Legacy | Done!"
